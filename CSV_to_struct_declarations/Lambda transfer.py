ffb9b6314182d9ba23dabe63b60687c7832467db
Michael <michaelleoni356@gmail.com>
2024-03-30 07:20:19 -0600

    updated python script

--- CSV_to_struct_declarations/Lambda transfer.py
+++ CSV_to_struct_declarations/Lambda transfer.py
@@ -1,10 +1,15 @@
 f = open("Digital Card Deck - Sheet1.csv","r")
 
-f_out = open("New Lambda.txt","a")
-
-f.readline()
+i=1
 
 for line in f:
-    words = line.split(",");
-    words[1] = words[1].strip();
-    f_out.write('{'+'"'+words[0]+'"'+','+'"'+words[1]+'"'+","+'"'+"placeholder.wav"+'"'+'},\n')
+    words = line.split(",")
+    words[1] = words[1].strip()
+    f_out = open(str(i)+".txt","w")
+
+    f_out.write(words[0]+'\n')
+    f_out.write(words[1]+'\n')
+    f_out.write("placeholder.wav"+'\n')
+
+    f_out.close()
+    i+=1
