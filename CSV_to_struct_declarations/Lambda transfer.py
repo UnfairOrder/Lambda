a463fb4d77502e5f8474144fa839323382417803
Michael <michaelleoni356@gmail.com>
2024-04-25 01:43:17 -0600

    Updated the python file to parse the audio files into ones that have compatible file names for the TMRpcm library A few lines in the csv were spelt wrong as well. One had an apostrophe.

--- CSV_to_struct_declarations/Lambda transfer.py
+++ CSV_to_struct_declarations/Lambda transfer.py
@@ -1,3 +1,5 @@
+import os
+from shutil import copy
 f = open("Digital Card Deck - Sheet1.csv","r")
 Audio_file_dir = "Audio"
 i=1
@@ -7,9 +9,20 @@ for line in f:
     words[1] = words[1].strip()
     f_out = open(str(i)+".txt","w")
 
+    audio_base = words[3].rstrip(".wav\n")
+    old_file_path = "Old_name_Audio/"+words[3].rstrip('\n')
+    copy(old_file_path,"Audio/")
+    old_file = os.path.join("Audio",words[3].rstrip('\n'))
+    new_file = os.path.join("Audio",audio_base[0:8]+".wav")
+    try:
+        os.rename(old_file,new_file)
+    except FileExistsError:
+        new_file = new_file.rstrip(".wav")+"1"+".wav"
+
     f_out.write(words[0]+'\n')
     f_out.write(words[1]+'\n')
-    f_out.write(Audio_file_dir+'/'+words[3]+'\n')
+
+    f_out.write(new_file+'\n')
 
     f_out.close()
     i+=1
